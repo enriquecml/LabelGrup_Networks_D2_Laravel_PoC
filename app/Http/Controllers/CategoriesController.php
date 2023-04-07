7296956e8b6b8a4edeac1f87165fcf0fa9451369
enriquecml <emesiasl@alumnos.unex.es>
2023-04-07 10:26:57 +0200

    + API Categorias

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\CategoryCollection;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
+use Illuminate\Http\Response;
+
+
+class CategoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories= QueryBuilder::for(Category::class)
+        ->allowedFields(['id','name'])
+        ->allowedFilters(['name'])
+        ->allowedSorts(['name'])
+        ->paginate();
+
+        return new CategoryCollection($categories);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $category = Category::create([
+            'name'=>$request->name
+        ]);
+
+        return (new CategoryResource($category))->response()
+        ->setStatusCode(Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        $category_f= QueryBuilder::for(Category::class)
+        ->allowedFields('id','name')
+        ->where('id',$category->id)
+        ->first()
+        ;
+       return new CategoryResource($category_f);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Category $category)
+    {
+        $category->update([
+            'name'=>$request->name
+        ]);
+
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\ProductsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,4 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::middleware('auth:sanctum')->group(function(){
     Route::apiResource('products',ProductsController::class);
+    Route::apiResource('categories',CategoriesController::class);
 });
